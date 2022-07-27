2ca6113803037d531346f90bbae37b357c6e67c7
Maryia Varabyeva <maryia.varabyeva@gmail.com>
2022-07-27 20:14:48 +0300

    feat: add the location of the elements

--- game/src/index.js
+++ game/src/index.js
@@ -17,3 +17,38 @@ function createBtns() {
   }
 }
 createBtns();
+
+const items = Array.from(document.querySelectorAll(".item"));
+const countItems = 16;
+
+if (items.length !== 16) {
+  throw new Error(`Должно быть ровно ${countItems} items в HTML`);
+}
+
+function getMatrix(arr) {
+  const size = 4;
+  let subArray = [];
+  for (let i = 0; i < Math.ceil(arr.length / size); i++) {
+    subArray[i] = arr.slice((i * size), (i * size) + size);
+  }
+  return subArray;
+}
+let matrix = getMatrix(
+  items.map((item) => Number(item.dataset.id))
+);
+
+function setNodeStyles(node, x, y) {
+  const shiftPs = 100;
+  node.style.transform = `translate3D(${x * shiftPs}%, ${y * shiftPs}%, 0)`;
+}
+
+function setPositionItems(matrix) {
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[y].length; x++) {
+      const value = matrix[y][x];
+      const node = items[value - 1];
+      setNodeStyles(node, x, y);
+    }
+  }
+}
+setPositionItems(matrix);
